980042d0be1f15085e98ecbf34b1ad395018fb31
Leo Sporn <spornleo@gmail.com>
2020-01-11 18:30:17 -0800

    - created key.json and imported google.cloud nlp toolkit in nlp.py

---
+++ nlp.py
@@ -0,0 +1,22 @@
+# Imports the Google Cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+if __name__ == '__main__':
+    print('test cloud')
+
+    # Instantiates a client
+    client = language.LanguageServiceClient.from_service_account_json('key.json')
+
+    # The text to analyze
+    text = u'Terrible work!'
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
+
+    # Detects the sentiment of the text
+    sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+    print('Text: {}'.format(text))
+    print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
