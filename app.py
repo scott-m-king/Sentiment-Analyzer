8c8518f11c28b057620397ba58d0aea73adb2640
Scott <scott.m.king@outlook.com>
2020-01-12 00:56:15 -0800

    Removed socialMedia from app.py and removed drop down from index.html

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 searchterms = []
-socialmedias = []
 
 
 # Homepage
@@ -19,9 +18,7 @@ socialmedias = []
 def hello_world():
     if request.method == "POST":
         searchterm = request.form.get("searchTerms")
-        socialmedia = request.form.get("socialMedia")
         searchterms.append(searchterm)
-        socialmedias.append(socialmedia)
         return redirect("/results")
     else:
         return render_template("index.html")
@@ -38,9 +35,9 @@ def results():
     sentiment_scores = [sentiment.sentiment.score for sentiment in sentiments]
     sentiment_magnitude = [sentiment.sentiment.magnitude for sentiment in sentiments]
 
-    return render_template("results.html", searchterm=searchterms[-1], socialmedia=socialmedias[-1],
-                           sentiment_names=sentiment_names, sentiment_saliences=sentiment_saliences,
-                           sentiment_scores=sentiment_scores, sentiment_magnitude=sentiment_magnitude)
+    return render_template("results.html", searchterm=searchterms[-1], sentiment_names=sentiment_names,
+                           sentiment_saliences=sentiment_saliences, sentiment_scores=sentiment_scores,
+                           sentiment_magnitude=sentiment_magnitude)
 
 
 if __name__ == '__main__':
