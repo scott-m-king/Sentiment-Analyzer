804ba2b22a5827663c81a04ddc77a075c5c6ad29
Leo Sporn <spornleo@gmail.com>
2020-01-11 21:16:49 -0800

    - implemented nlp functions in nlp.py, with documentation

--- nlp.py
+++ nlp.py
@@ -1,90 +1,140 @@
 # Imports the Google Cloud client library
 from google.cloud import language
 from google.cloud.language import enums
-from google.cloud.language import types
+
+
+def analyze_sentiment(text: str):
+    """
+    Performs a sentiment analysis on the input string.
+
+    Returns magnitude and score of sentiment.
+
+    :param text:
+    Input string.
+
+    :return:
+    sentiment (type google.cloud.languave_v1.types.AnalyzeSentimentResponse).
+
+    sentiment.document_sentiment
+        - measure of sentiment of whole document
+        - fields:
+            magnitude
+            score
+
+    sentiment.sentences
+        - list of sentences in the document, each of which has magnitude and score fields
+    """
+    return __analyze(text, 'sentiment')
+
+
+def analyze_entities(text: str):
+    """
+    Performs an entity analysis on the input string.
+
+    Returns all the entities referred to in the string.
+
+    :param text:
+    Input string.
+
+    :return:
+    Entities (type google.cloud.language_v1.types.AnalyzeEntitiesResponse).
+
+    entities.entities
+        - list of entities with fields:
+            - name
+            - type
+            - salience
+            - mentions
+    """
+    return __analyze(text, 'entities')
+
+
+def analyze_entity_sentiment(text: str):
+    """
+    Performs a combination entity/sentiment analysis on the input text.
+
+    Returns all entity/sentiments referred to in the string.
+
+    :param text:
+    Input string.
+
+    :return:
+    entity_sentiment (type google.cloud.language_v1.types.AnalyzeEntitySentimentResponse).
+
+    entity_sentiment.entities
+        - list of entities with fields:
+            - name
+            - type
+            - salience
+            - mentions
+            - sentiment
+                - magnitude
+                - score
+    """
+    return __analyze(text, 'entity-sentiment')
+
+
+def analyze_syntax(text: str):
+    """
+    Performs syntax analysis on the input string.
+
+    Returns syntax for each token in the sentence.
+
+    :param text:
+    Input string.
+
+    :return:
+    entity_sentiment (type google.cloud.language_v1.types.AnalyzeSyntaxResponse).
+    """
+    return __analyze(text, 'syntax')
+
+
+def classify_text(text: str):
+    """
+    Performs a classification of the input string.
+
+    :param text:
+    Input string.
+
+    :return:
+    classification (type google.cloud.language_v1.types.ClassifyTextResponse).
+
+    classification.categories
+        - list of categories with fields
+            - name
+            - confidence
+
+    """
+    return __analyze(text, 'classification')
+
+
+def __analyze(text: str, analysis_level: str):
+    client = language.LanguageServiceClient.from_service_account_json('key.json')
+    document = {"content": text, "type": enums.Document.Type.PLAIN_TEXT, "language": 'en'}
+    analyses = {'sentiment': client.analyze_sentiment,
+                'entities': client.analyze_entities,
+                'entity-sentiment': client.analyze_entity_sentiment,
+                'syntax': client.analyze_syntax,
+                'classification': client.classify_text}
+    try:
+        result = analyses[analysis_level.lower()](document)
+        return result
+    except KeyError:
+        raise NotImplementedError
+
 
 if __name__ == '__main__':
+    test_text = 'I’m a high school student in Nanaimo currently choosing between UBC and VIU. ' \
+                'My parents are putting aside 20k for me. ' \
+                'If I go to VIU, I will live with my parents and I won’t have to work or pay for food. ' \
+                'I’ll graduate with no debt. ' \
+                'If I go to UBC, I’ll be over 100k in debt at graduation. ' \
+                'I’m sick of Nanaimo and love the city. ' \
+                'I’m also pretty diligent when it comes to academic success, and love competition. ' \
+                'What seems like the better school in my circumstance?'
 
-    # Instantiates a client
-    client = language.LanguageServiceClient.from_service_account_json('key.json')
-
-    # The text to analyze
-    text = 'Terrible work!'
-    document = types.Document(
-        content=text,
-        type=enums.Document.Type.PLAIN_TEXT)
-
-    # Detects the sentiment of the text
-    sentiment = client.analyze_sentiment(document=document).document_sentiment
-
-
-    from google.cloud import language_v1
-    from google.cloud.language_v1 import enums
-
-
-    def sample_analyze_syntax(text_content):
-        """
-        Analyzing Syntax in a String
-
-        Args:
-          text_content The text content to analyze
-        """
-
-        client = language_v1.LanguageServiceClient.from_service_account_json('key.json')
-
-        # text_content = 'This is a short sentence.'
-
-        # Available types: PLAIN_TEXT, HTML
-        type_ = enums.Document.Type.PLAIN_TEXT
-
-        # Optional. If not specified, the language is automatically detected.
-        # For list of supported languages:
-        # https://cloud.google.com/natural-language/docs/languages
-        language = "en"
-        document = {"content": text_content, "type": type_, "language": language}
-
-        # Available values: NONE, UTF8, UTF16, UTF32
-        encoding_type = enums.EncodingType.UTF8
-
-        response = client.analyze_syntax(document, encoding_type=encoding_type)
-        # Loop through tokens returned from the API
-        for token in response.tokens:
-            # Get the text content of this token. Usually a word or punctuation.
-            text = token.text
-            print(u"Token text: {}".format(text.content))
-            print(
-                u"Location of this token in overall document: {}".format(text.begin_offset)
-            )
-            # Get the part of speech information for this token.
-            # Parts of spech are as defined in:
-            # http://www.lrec-conf.org/proceedings/lrec2012/pdf/274_Paper.pdf
-            part_of_speech = token.part_of_speech
-            # Get the tag, e.g. NOUN, ADJ for Adjective, et al.
-            print(
-                u"Part of Speech tag: {}".format(
-                    enums.PartOfSpeech.Tag(part_of_speech.tag).name
-                )
-            )
-            # Get the voice, e.g. ACTIVE or PASSIVE
-            print(u"Voice: {}".format(enums.PartOfSpeech.Voice(part_of_speech.voice).name))
-            # Get the tense, e.g. PAST, FUTURE, PRESENT, et al.
-            print(u"Tense: {}".format(enums.PartOfSpeech.Tense(part_of_speech.tense).name))
-            # See API reference for additional Part of Speech information available
-            # Get the lemma of the token. Wikipedia lemma description
-            # https://en.wikipedia.org/wiki/Lemma_(morphology)
-            print(u"Lemma: {}".format(token.lemma))
-            # Get the dependency tree parse information for this token.
-            # For more information on dependency labels:
-            # http://www.aclweb.org/anthology/P13-2017
-            dependency_edge = token.dependency_edge
-            print(u"Head token index: {}".format(dependency_edge.head_token_index))
-            print(
-                u"Label: {}".format(enums.DependencyEdge.Label(dependency_edge.label).name)
-            )
-
-        # Get the language of the text, which will be the same as
-        # the language specified in the request or, if not specified,
-        # the automatically-detected language.
-        print(u"Language of the text: {}".format(response.language))
-
-    sample_analyze_syntax("Great job, everyone!")
+    sentiment = analyze_sentiment(test_text)
+    entities = analyze_entities(test_text)
+    entity_sentiment = analyze_entity_sentiment(test_text)
+    syntax = __analyze(test_text, 'syntax')
+    classification = __analyze(test_text, 'classification')
