1eb04a91cb9445705e19dd8a2d272eb9a70c8c05
Scott <scott.m.king@outlook.com>
2020-01-11 21:44:39 -0800

    Created results page and added some backend logic

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask import Flask, flash, jsonify, redirect, render_template, request
 
 app = Flask(__name__)
 
@@ -6,10 +6,26 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
+searchterms = []
+
+
 # Homepage
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def hello_world():
-    return render_template("index.html")
+    if request.method == "POST":
+        searchterm = request.form.get("searchTerms")
+        searchterms.append(searchterm)
+        print(searchterm)
+        return redirect("/results")
+    else:
+        return render_template("index.html")
+
+
+# Results page
+@app.route('/results', methods=["GET", "POST"])
+def results():
+    print(searchterms)
+    return render_template("results.html", searchterm=searchterms[-1])
 
 
 if __name__ == '__main__':
